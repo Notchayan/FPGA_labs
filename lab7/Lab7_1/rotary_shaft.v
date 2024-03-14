cd2aaa9ec04a2cec8fe9cbd7c021628a2b593919
Chayan Kumawat <130827975+Notchayan@users.noreply.github.com>
2024-03-14 19:28:41 +0000

    Update rotary_shaft.v

--- lab7/Lab7_1/rotary_shaft.v
+++ lab7/Lab7_1/rotary_shaft.v
@@ -1,35 +1,20 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    15:31:24 03/13/2019 
-// Design Name: 
-// Module Name:    rotary_shaft 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module rotary_shaft(clk, ROT_A, ROT_B, rotation_event);
-input clk, ROT_A, ROT_B;
-output rotation_event;
-reg rotation_event;
+ 
+module RotaryShaftDetector(
+    input clk, // Clock input
+    input rotation_A, // Rotary shaft signal A
+    input rotation_B, // Rotary shaft signal B
+    output reg rotation_event // Output indicating rotation event
+);
 
-always@(posedge clk) begin
-	if(ROT_A == 1 & ROT_B == 1) begin
-		rotation_event <= 1;
-	end
-	else if(ROT_A == 0 & ROT_B == 0) begin
-		rotation_event <= 0;
-	end
-end
+    // Detect rotation event based on the states of rotation signals A and B
+    always @(posedge clk) begin
+        if (rotation_A == 1 && rotation_B == 1) begin
+            rotation_event <= 1; // Set rotation event signal high
+        end
+        else if (rotation_A == 0 && rotation_B == 0) begin
+            rotation_event <= 0; // Set rotation event signal low
+        end
+    end
 
 endmodule
