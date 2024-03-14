514b2493335d9db83d23eec7d6285908561508c4
Chayan Kumawat <130827975+Notchayan@users.noreply.github.com>
2024-03-14 19:32:12 +0000

    Update lcd_top.v

--- lab7/Lab7_2/lcd_top.v
+++ lab7/Lab7_2/lcd_top.v
@@ -1,76 +1,62 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:00:55 03/13/2019 
-// Design Name: 
-// Module Name:    lcd_top 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module lcd_top(clk, x, buttons, lcd_rs, lcd_rw, lcd_e, lcd4, lcd5, lcd6, lcd7);
-input clk;
-input [2:0] x;
-input [3:0] buttons;
-output lcd_rs, lcd_rw, lcd_e, lcd4, lcd5, lcd6, lcd7;
-wire lcd_rs, lcd_rw, lcd_e, lcd4, lcd5, lcd6, lcd7;
-reg [2:0] a, b, c, d;
-wire [1:0] min_pos;
 
-reg [7:0] ascii[7:0]; 
-initial begin
-	ascii[0] = 8'd48;
-	ascii[1] = 8'd49;
-	ascii[2] = 8'd50;
-	ascii[3] = 8'd51;
-	ascii[4] = 8'd52;
-	ascii[5] = 8'd53;
-	ascii[6] = 8'd54;
-	ascii[7] = 8'd55;
-end
-reg [0:127] first_line;
-reg [0:127] second_line;
-
-always@(posedge buttons[0]) begin
-	a <= x;
-end
-always@(posedge buttons[1]) begin
-	b <= x;
-end
-always@(posedge buttons[2]) begin
-	c <= x;
-end
-always@(posedge buttons[3]) begin
-	d <= x;
-end
-returnMin MIN(a, b, c, d, min_pos);
-
-always@(posedge clk) begin
-	first_line <= {ascii[a], 8'd44, 8'd32, ascii[b], 8'd44, 8'd32, ascii[c], 8'd44, 8'd32, ascii[d], 48'd0};
-	if(min_pos == 0) begin
-		second_line <= {ascii[0], 120'd0};
-	end
-	else if(min_pos == 1) begin
-		second_line <= {ascii[1], 120'd0};
-	end
-	else if(min_pos == 2) begin
-		second_line <= {ascii[2], 120'd0};
-	end
-	else begin
-		second_line <= {ascii[3], 120'd0};
-	end
-end
-
-lcd LCD (first_line, second_line, clk, lcd_rs, lcd_rw, lcd_e, lcd4, lcd5, lcd6, lcd7);
+module LCD_Controller_Top(
+    input clk, // Clock input
+    input [2:0] button_input, // Input for selecting buttons
+    input [3:0] button_values, // Values of the buttons
+    output lcd_rs, lcd_rw, lcd_e, lcd4, lcd5, lcd6, lcd7 // LCD control signals
+);
+    // Control signals for LCD
+    output reg lcd_rs, lcd_rw, lcd_e, lcd4, lcd5, lcd6, lcd7;
+    
+    // Register declarations for button inputs
+    reg [2:0] button_a, button_b, button_c, button_d; 
+    
+    // Wire declaration for determining the minimum position among buttons
+    wire [1:0] min_position;
+    
+    // ASCII values for numbers 0 to 7
+    reg [7:0] ascii[7:0]; 
+    initial begin
+        ascii[0] = 8'd48; // ASCII value for '0'
+        ascii[1] = 8'd49; // ASCII value for '1'
+        ascii[2] = 8'd50; // ASCII value for '2'
+        ascii[3] = 8'd51; // ASCII value for '3'
+        ascii[4] = 8'd52; // ASCII value for '4'
+        ascii[5] = 8'd53; // ASCII value for '5'
+        ascii[6] = 8'd54; // ASCII value for '6'
+        ascii[7] = 8'd55; // ASCII value for '7'
+    end
+    
+    // Register declarations for storing first and second line data
+    reg [0:127] first_line_data;
+    reg [0:127] second_line_data;
+    
+    // Assigning button inputs to respective registers
+    always @(posedge button_values[0])
+        button_a <= button_input;
+    always @(posedge button_values[1])
+        button_b <= button_input;
+    always @(posedge button_values[2])
+        button_c <= button_input;
+    always @(posedge button_values[3])
+        button_d <= button_input;
+    
+    // Finding the minimum position among button inputs
+    Return_Minimum_Position MIN_POS(button_a, button_b, button_c, button_d, min_position);
+    
+    // Assigning data for first and second lines based on button inputs
+    always @(posedge clk) begin
+        first_line_data <= {ascii[button_a], 8'd44, 8'd32, ascii[button_b], 8'd44, 8'd32, ascii[button_c], 8'd44, 8'd32, ascii[button_d], 48'd0};
+        case (min_position)
+            0: second_line_data <= {ascii[0], 120'd0}; // Display ASCII value for '0' on second line
+            1: second_line_data <= {ascii[1], 120'd0}; // Display ASCII value for '1' on second line
+            2: second_line_data <= {ascii[2], 120'd0}; // Display ASCII value for '2' on second line
+            default: second_line_data <= {ascii[3], 120'd0}; // Display ASCII value for '3' on second line
+        endcase
+    end
+    
+    // Instantiating the LCD controller module
+    LCD_Controller LCD_Module(first_line_data, second_line_data, clk, lcd_rs, lcd_rw, lcd_e, lcd4, lcd5, lcd6, lcd7);
 
 endmodule
