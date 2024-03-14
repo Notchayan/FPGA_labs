0edc16a1be966d357721afee0b690b425dbaa9d5
Chayan Kumawat <130827975+Notchayan@users.noreply.github.com>
2024-03-14 19:33:59 +0000

    Update returnMin.v

--- lab7/Lab7_2/returnMin.v
+++ lab7/Lab7_2/returnMin.v
@@ -1,64 +1,49 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:01:28 03/13/2019 
-// Design Name: 
-// Module Name:    returnMin 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module returnMin(a, b, c, d, min_pos);
-input [2:0] a, b, c, d;
-output [1:0] min_pos;
-reg min_pos;
 
-always@(a, b, c, d) begin
-	if(a < b) begin
-		if(a < c) begin
-			if(a < d) begin
-				min_pos <= 0;
-			end
-			else begin
-				min_pos <= 3;
-			end
-		end
-		else begin
-			if(c < d) begin
-				min_pos <= 2;
-			end
-			else begin
-				min_pos <= 3;
-			end
-		end
-	end
-	else begin
-		if(b < c) begin
-			if(b < d) begin
-				min_pos <= 1;
-			end
-			else begin
-				min_pos <= 3;
-			end
-		end
-		else begin
-			if(c < d) begin
-				min_pos <= 2;
-			end
-			else begin
-				min_pos <= 3;
-			end
-		end
-	end
-end
+module Minimum_Position(
+    input [2:0] input_a, input_b, input_c, input_d, // Input values to compare
+    output reg [1:0] min_position // Output indicating the minimum position
+);
+    // Register declaration for storing the minimum position
+    reg [1:0] min_position;
+
+    // Always block to determine the minimum position among the inputs
+    always @(*) begin
+        if (input_a < input_b) begin
+            if (input_a < input_c) begin
+                if (input_a < input_d) begin
+                    min_position = 2'b00; // Input 'a' is the minimum
+                end
+                else begin
+                    min_position = 2'b11; // Input 'd' is the minimum
+                end
+            end
+            else begin
+                if (input_c < input_d) begin
+                    min_position = 2'b10; // Input 'c' is the minimum
+                end
+                else begin
+                    min_position = 2'b11; // Input 'd' is the minimum
+                end
+            end
+        end
+        else begin
+            if (input_b < input_c) begin
+                if (input_b < input_d) begin
+                    min_position = 2'b01; // Input 'b' is the minimum
+                end
+                else begin
+                    min_position = 2'b11; // Input 'd' is the minimum
+                end
+            end
+            else begin
+                if (input_c < input_d) begin
+                    min_position = 2'b10; // Input 'c' is the minimum
+                end
+                else begin
+                    min_position = 2'b11; // Input 'd' is the minimum
+                end
+            end
+        end
+    end
 endmodule
