2db66ac5e0899331bd392726b10e51e94bc04fb4
Chayan Kumawat <130827975+Notchayan@users.noreply.github.com>
2024-03-14 19:24:22 +0000

    Update fsmROM.v

--- lab7/Lab7_1/fsmROM.v
+++ lab7/Lab7_1/fsmROM.v
@@ -1,66 +1,53 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    15:30:29 03/13/2019 
-// Design Name: 
-// Module Name:    fsmROM 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module fsmROM(clk, x, curr_state, next_state);
-input clk;
-input [1:0] x;
-input [3:0] curr_state;
-output [3:0] next_state;
-reg [3:0] next_state;
-reg [2:0] mrom[12:0];
-reg [3:0] drom1[3:0], drom2[3:0];
-reg counter = 0;
 
-initial begin
-	drom1[0] <= 4'b0100;
-	drom1[1] <= 4'b0101;
-	drom1[2] <= 4'b0110;
-	drom1[3] <= 4'b0110;
+module FiniteStateMachine (
+    input clk,
+    input [1:0] input_signal,
+    input [3:0] current_state,
+    output reg [3:0] next_state
+);
 
-	drom2[0] <= 4'b1011;
-	drom2[1] <= 4'b1100;
-	drom2[2] <= 4'b1100;
-	drom2[3] <= 4'b1100;
+    // Define states and transitions
+    reg [3:0] next_state;
+    reg [2:0] state_transition_ROM[12:0];
+    reg [3:0] state_data_ROM1[3:0], state_data_ROM2[3:0];
+    reg counter = 0;
 
-	mrom[0] <= 3'b000;
-	mrom[1] <= 3'b000;
-	mrom[2] <= 3'b000;
-	mrom[3] <= 3'b001;
-	mrom[4] <= 3'b010;
-	mrom[5] <= 3'b010;
-	mrom[6] <= 3'b000;
-	mrom[7] <= 3'b000;
-	mrom[8] <= 3'b000;
-	mrom[9] <= 3'b000;
-	mrom[10] <= 3'b011;
-	mrom[11] <= 3'b100;
-	mrom[12] <= 3'b100;
-end
+    // Initialize state transition and data ROMs
+    initial begin
+        state_data_ROM1[0] <= 4'b0100;
+        state_data_ROM1[1] <= 4'b0101;
+        state_data_ROM1[2] <= 4'b0110;
+        state_data_ROM1[3] <= 4'b0110;
 
-always@(posedge clk) begin
-	case(mrom[curr_state])
-		3'b000: next_state <= curr_state + 1;
-		3'b001: next_state <= drom1[x];
-		3'b010: next_state <= 7;
-		3'b011: next_state <= drom2[x];
-		3'b100: next_state <= 0;
-	endcase
-end
+        state_data_ROM2[0] <= 4'b1011;
+        state_data_ROM2[1] <= 4'b1100;
+        state_data_ROM2[2] <= 4'b1100;
+        state_data_ROM2[3] <= 4'b1100;
+
+        state_transition_ROM[0] <= 3'b000;
+        state_transition_ROM[1] <= 3'b000;
+        state_transition_ROM[2] <= 3'b000;
+        state_transition_ROM[3] <= 3'b001;
+        state_transition_ROM[4] <= 3'b010;
+        state_transition_ROM[5] <= 3'b010;
+        state_transition_ROM[6] <= 3'b000;
+        state_transition_ROM[7] <= 3'b000;
+        state_transition_ROM[8] <= 3'b000;
+        state_transition_ROM[9] <= 3'b000;
+        state_transition_ROM[10] <= 3'b011;
+        state_transition_ROM[11] <= 3'b100;
+        state_transition_ROM[12] <= 3'b100;
+    end
+
+    // State transition logic
+    always @(posedge clk) begin
+        case(state_transition_ROM[current_state])
+            3'b000: next_state <= current_state + 1;
+            3'b001: next_state <= state_data_ROM1[input_signal];
+            3'b010: next_state <= 7;
+            3'b011: next_state <= state_data_ROM2[input_signal];
+            3'b100: next_state <= 0;
+        endcase
+    end
 endmodule
